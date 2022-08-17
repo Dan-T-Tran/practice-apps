9de45bbfd8464c4c7dba3a100139ba95ca0e810f
Nooby <dan.t.tran505@gmail.com>
2022-08-17 09:50:59 -0700

    glossary back-end skeleton

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,4 +1,32 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+// Maybe change port number if necessary
+// Maybe change /test if necessary
+
+const wordSchema = new mongoose.Schema({
+  word: String,
+  definition: String
+});
+
+const Word = wordSchema.model('Words', wordSchema);
+
+module.exports = {
+  postWord: function() {
+
+  },
+
+  searchWord: function() {
+
+  },
+
+  updateWord: function() {
+
+  },
+
+  deleteWord: function() {
+
+  }
+};
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -2,18 +2,31 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+const {postWord, searchWord, updateWord, deleteWord} = require('./db.js');
+
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+//If not using express.json, maybe try stringifying data client-side before sending to server?
+
+app.post('/glossary', (req, res) => {
+
+});
+
+app.get('/glossary', (req, res) => {
+
+});
+
+app.put('/glossary', (req, res) => {
+
+});
+
+app.delete('/glossary', (req, res) => {
+
+});
+
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
